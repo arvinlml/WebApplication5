a5326bbdb9fcf24652f6be63f4dc18ccaf324a92
Aravindh Janakiraman <Aravindh.Janakiraman@wu.com>
2019-10-08 02:26:38 -0400

    Add project files.

---
+++ Models/BankNarratives.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication5.Models
+{
+    [Table("BANK_NARRATIVES")]
+    public class BankNarratives
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Column("REPORT_ID")]
+        public int ReportId { get; set; }
+
+        [Column("BANK_NARRATIVES")]
+        [MaxLength(19000)]
+        public string BankNarrative { get; set; }
+
+        [Column("CREATE_USER_ID")]
+        public int? CreateUserId { get; set; }
+
+        [Column("CREATE_TIMESTAMP")]
+        public DateTime? CreateTimestamp { get; set; }
+
+        [Column("MODIFIED_USER_ID")]
+        public int? ModifiedUserId { get; set; }
+
+        [Column("MODIFIED_TIMESTAMP")]
+        public DateTime? ModifiedTimestamp { get; set; }
+    }
+}
